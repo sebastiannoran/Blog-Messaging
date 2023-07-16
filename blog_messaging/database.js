178d70666a61ac0d89df4625309281c0408915b6
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-07-16 18:32:38 -0400

    dequelize

--- blog_messaging/database.js
+++ blog_messaging/database.js
@@ -1,4 +1,11 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-console.log(process.env.DB_USER);
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  })
+
