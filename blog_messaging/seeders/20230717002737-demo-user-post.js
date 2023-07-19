0ab750d14e1163ccecad9cd3aca4a0a930da818e
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-07-18 21:36:15 -0400

    one user can have many comments relationship

--- blog_messaging/seeders/20230717002737-demo-user-post.js
+++ blog_messaging/seeders/20230717002737-demo-user-post.js
@@ -1,4 +1,7 @@
 'use strict';
+
+const user = require('../models/user');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -43,6 +46,7 @@ module.exports = {
           createdAt: new Date(),
           updatedAt: new Date(),
           postId: postId,
+          userId:userId,
         }
       ],
       {}
